1362fe54ce45046d957f833943cb63d3f8c02f51
ELEliseev <eliseevlifter@gmail.com>
2022-10-29 21:40:09 +0300

    fix list-subset

--- list-subset/main.dart
+++ list-subset/main.dart
@@ -1,4 +1,12 @@
-bool listSubset(List<int> source, List<int> subset) {}
+bool listSubset(List<int> source, List<int> subset) {
+  if (source.length < subset.length) {
+    return false;
+  }
+  List<int> sortedSource = source..sort();
+  List<int> sortedSubset = subset..sort();
+
+  return sortedSource.join().contains(sortedSubset.join());
+}
 
 main() {
   print(listSubset([2, 1, 3], [1, 2, 3])); // -> true
