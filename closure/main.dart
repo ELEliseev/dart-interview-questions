66324bf414ca3798234beaf372abe32e2ed25477
ELEliseev <eliseevlifter@gmail.com>
2022-10-29 21:13:15 +0300

    fix closure

--- closure/main.dart
+++ closure/main.dart
@@ -1,9 +1,20 @@
-Function increment = (){
+Function increment = (() {
 //todo  IIFE (immediately invoked function expression) and closure
-}
 
+  int count = 0;
 
+  return () => (count += 1);
+})();
+
+//Function getCount = increment();
 main() {
   print(increment()); //1
   print(increment()); //2
+  print(increment()); //3
+  print(increment()); //4
+
+  // print(getCount());
+  // print(getCount());
+  // print(getCount());
+  // print(getCount());
 }
