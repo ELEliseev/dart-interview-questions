f91c98ece4da6705d2a4f3bb82a5e9e3d1464ac7
ELEliseev <eliseevlifter@gmail.com>
2022-10-29 20:49:01 +0300

    fix search

--- search/main.dart
+++ search/main.dart
@@ -1,8 +1,10 @@
-bool search(List<dynamic> list, dynamic element) {}
+int search(List<int> list, int element) {
+  return list.indexOf(element);
+}
 
 main() {
   print(search([1, 3, 6, 13, 17], 13)); // -> 3
   print(search([1, 3, 6, 13, 17], 12)); // -> -1
-  print(search([1, 3, 6, 13, 17], 1)); // -> 3
-  print(search([1, 3, 6, 13, 17], 1)); // -> -1
+  print(search([1, 3, 6, 13, 17], 1)); // -> 0
+  print(search([1, 3, 6, 13, 17], 18)); // -> -1
 }
