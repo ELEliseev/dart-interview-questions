6c4800b1d4aafe6fbbb07b80e471b9a7f96ebd6a
ELEliseev <eliseevlifter@gmail.com>
2022-10-29 20:04:00 +0300

    fix add

--- add/main.dart
+++ add/main.dart
@@ -1,5 +1,19 @@
 int add(List<int> args) {
   //вернуть сумму списка
+  // int out = 0;
+
+  // for (int i = 0; i <= args.length - 1; i++) {
+  //   out = out + args[i];
+  // }
+  // return out;
+
+  return args.reduce((value, element) => value + element);
+
+  //value 0---1-->1
+  //value 1---2-->3
+  //value 3---3-->6
+  //value 6---4-->10
+  //reduce,map,filter,some,every
 }
 
 void main() {
