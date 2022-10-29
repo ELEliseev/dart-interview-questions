a24709429f16262b1b6e1bcdf734d54fe5f3e313
ELEliseev <eliseevlifter@gmail.com>
2022-10-29 21:30:16 +0300

    fix flutten

--- flutten/main.dart
+++ flutten/main.dart
@@ -1,7 +1,32 @@
-List<dynamic> flatten(List<dynamic> list) {
-  //вернуть плоский список
+List<int> flatten(List<dynamic> list) {
+  //вернуть плоский список  recursive Рекурсия!
+  List<int> fluttenList = [];
+
+  list.forEach((element) {
+    if (element is List) {
+      var flats = flatten(element);
+      fluttenList.addAll(flats);
+
+      return;
+    }
+
+    fluttenList.add(element);
+  });
+
+  return fluttenList;
 }
 
 void main() {
-  //print(flatten([1,[2],[ [3, 4] ], [[ [5] ] ] ])); // -> [1, 2, 3, 4,5]
+  print(flatten([
+    1,
+    [2],
+    [
+      [3, 4]
+    ],
+    [
+      [
+        [5]
+      ]
+    ]
+  ])); // -> [1, 2, 3, 4, 5]
 }
