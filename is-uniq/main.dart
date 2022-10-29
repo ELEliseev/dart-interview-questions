0fa9d4643b8ff47d1733a829cfd521489171a816
ELEliseev <eliseevlifter@gmail.com>
2022-10-29 20:58:56 +0300

    fix is-uniq

--- is-uniq/main.dart
+++ is-uniq/main.dart
@@ -1,7 +1,12 @@
 bool isUnique(String str) {
-  // todo
+  // todo Set
+  Set<String> uniqStr = Set.from(str.split(''));
+  // print('$str, $uniqStr');
+
+  return str.length == uniqStr.length;
 }
 
+// List,Set,Map
 main() {
   print(isUnique("abcdef")); // -> true
   print(isUnique("1234567")); // -> true
